6d8e867c4943d7083a1945972f29dde6e3e8b5ea
IlimSarykbaev <ilim.sarykbaev@gmail.com>
2020-10-27 07:31:42 +0600

    Initial commit

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,52 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+    public static void main(String[] args) throws SQLException {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        UserService userService = context.getBean(UserService.class);
+
+        User user5 = new User("User5", "Lastname5", "user5@mail.ru");
+        userService.add(user5);
+        Car car5 = new Car("CarForUser5", 5);
+        user5.setCar(car5);
+
+
+       /* userService.add(new Car("Gamma", 3));
+        userService.add(new Car("Delta", 4));
+
+        List<Car> cars = userService.listCars();
+        for (Car car : cars) {
+            System.out.println("Id = " + car.getId());
+            System.out.println("Car's Model = " + car.getModel());
+            System.out.println("Car's Series = " + car.getSeries());
+            System.out.println();
+        }*/
+
+        /*userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
+        userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
+        userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
+        userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+
+        List<User> users = userService.listUsers();
+        for (User user : users) {
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
+            System.out.println();
+        }*/
+
+        context.close();
+    }
+}
